1ca911a2aa317b638c1862f8dbf738bf19a69140
KeithConroy <kconroyjr@gmail.com>
2017-12-17 11:48:54 -0800

    create

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -7,11 +7,27 @@ class AnimalsController < ApplicationController
     @animal = Animal.new
   end
 
+  def create
+    @animal = Animal.new(animal_params)
+
+    if @animal.save
+      redirect_to animal_path(@animal.id)
+    else
+      render json: @animal.errors.full_messages, status: 400
+    end
+  end
+
   def show
     @animal = Animal.find(params[:id])
   end
 
   def edit
     @animal = Animal.find(params[:id])
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:first_name, :gender, :bio, :identification_number, :arrived_at_shelter, :age, :is_good_with_kids, :is_good_with_dogs, :is_good_with_cats, :size, :color, :animal_type, :breed)
   end
 end
--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,11 +7,27 @@ class SheltersController < ApplicationController
     @shelter = Shelter.new
   end
 
+  def create
+    @shelter = Shelter.new(shelter_params)
+
+    if @shelter.save
+      redirect_to shelter_path(@shelter.id)
+    else
+      render json: @shelter.errors.full_messages, status: 400
+    end
+  end
+
   def show
     @shelter = Shelter.find(params[:id])
   end
 
   def edit
     @shelter = Shelter.find(params[:id])
+  end
+
+  private
+
+  def shelter_params
+    params.require(:shelter).permit(:address_line_1, :address_line_2, :phone_number, :email, :zip_code, :city, :state, :org_name, :subdomain)
   end
 end
